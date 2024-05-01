da612ed034e4a2926f699b3ea05bf090e4230a57
zinov <zinov@DESKTOP-3Q1VDF0>
2024-05-01 14:47:52 +0300

    Add project files.

---
+++ HM_Database-First/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_Database_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ShopEntities()) { 
+                foreach (var item in db.Products)
+                {
+                    Console.WriteLine(item);
+                }
+                db.Products.Add(new Product { Name = "test"});
+                db.SaveChanges();
+                Console.WriteLine("--------------After Add-------------");
+                foreach (var item in db.Products)
+                {
+                    Console.WriteLine(item);
+                }
+                db.Products.Remove(db.Products.First(i => i.Id == 4));
+                db.SaveChanges();
+                Console.WriteLine("------------After Remove------------");
+                foreach (var item in db.Products)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
---
+++ HM_Database-First/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HM_Database-First")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HM_Database-First")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac8100b5-528c-4e65-acf8-817386ba314b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
